c19a4e9f7d671af7ec960e2faad00c8465eef501
creatorlxd <1461637345@qq.com>
2021-12-27 16:07:57 +0800

    fix sth

--- LASM/LASMModule.h
+++ LASM/LASMModule.h
@@ -70,5 +70,5 @@ namespace LASM
 	std::map<std::string, uint64_t (*)(LLL::Byte*, uint64_t, uint64_t, uint64_t)> GetFunctionsFromModule(const std::string& module_name);
 }	 // namespace LASM
 
-#define LASM_MODULE LLL_DLL_EXPORT extern "C" void RegisterModule(std::map<const char*, uint64_t (*)(LLL::Byte*, uint64_t, uint64_t, uint64_t)>& re)
+#define LASM_MODULE extern "C" LLL_DLL_EXPORT void RegisterModule(std::map<const char*, uint64_t (*)(LLL::Byte*, uint64_t, uint64_t, uint64_t)>& re)
 #define LASM_MODULE_FUNCTION(name, func) re.insert(std::make_pair(name, func));
