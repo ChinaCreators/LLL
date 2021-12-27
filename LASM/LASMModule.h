5ce0c78a969ff54e0a6b1455271ca7955350ada6
creatorlxd <1461637345@qq.com>
2021-12-27 16:00:03 +0800

    fix sth

--- LASM/LASMModule.h
+++ LASM/LASMModule.h
@@ -59,10 +59,16 @@
 #define LLL_POSIX
 #endif
 
+#if defined(LLL_WINDOWS) && defined(LLL_MSVC)
+#define LLL_DLL_EXPORT __declspec(dllexport)
+#else
+#define LLL_DLL_EXPORT __attribute__((visibility("default")))
+#endif
+
 namespace LASM
 {
 	std::map<std::string, uint64_t (*)(LLL::Byte*, uint64_t, uint64_t, uint64_t)> GetFunctionsFromModule(const std::string& module_name);
 }	 // namespace LASM
 
-#define LASM_MODULE extern "C" void RegisterModule(std::map<const char*, uint64_t (*)(LLL::Byte*, uint64_t, uint64_t, uint64_t)>& re)
+#define LASM_MODULE LLL_DLL_EXPORT extern "C" void RegisterModule(std::map<const char*, uint64_t (*)(LLL::Byte*, uint64_t, uint64_t, uint64_t)>& re)
 #define LASM_MODULE_FUNCTION(name, func) re.insert(std::make_pair(name, func));
